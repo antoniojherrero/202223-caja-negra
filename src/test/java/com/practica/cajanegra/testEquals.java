500ffd40ae93e904892e5f64e4abdfa932cb73a0
antoniojherrero <antonio.jherrero@alumnos.upm.es>
2022-11-16 18:14:32 +0100

    equals() BinaryTree tests added

--- src/test/java/com/practica/cajanegra/testEquals.java
+++ src/test/java/com/practica/cajanegra/testEquals.java
@@ -24,4 +24,37 @@ public class testEquals {
 		
 		assertTrue(arbol.equals(tree1));
 	}
+	@Test
+	void testC2() {
+		BinaryTree<String> tree2 = new BinaryTree<String>("1");
+		Node<String> nodo2 = tree2.insert("1", tree2.getRoot(), false);
+		tree2.insert("1", tree2.getRoot(), true);
+		tree2.insert("1", nodo2, false);
+		tree2.insert("1", nodo2, true);
+		
+		assertFalse(arbol.equals(tree2));
+	}
+	@Test
+	void testC3() {
+		BinaryTree<String> tree3 = new BinaryTree<String>("1");
+		tree3.insert("1", tree3.getRoot(), true);
+		tree3.insert("1", tree3.getRoot(), false);
+		
+		assertTrue(arbol.equals(tree3));
+	}
+	@Test
+	void testC4() {
+		BinaryTree<String> tree4 = new BinaryTree<String>("1");
+		Node<String> nodo4 = tree4.insert("1", tree4.getRoot(), true);
+		tree4.insert("1", tree4.getRoot(), false);
+		tree4.insert("1", nodo4, true);
+		
+		assertFalse(arbol.equals(tree4));
+	}
+	@Test 
+	void testNC(){
+		assertThrows(java.lang.Exception.class, ()->{
+			arbol.equals(null);
+		});
+	}
 }
