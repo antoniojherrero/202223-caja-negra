dd0a7a3e75992eeb135b9c93623142e4d79452ab
antoniojherrero <antonio.jherrero@alumnos.upm.es>
2022-11-30 09:13:30 +0100

    cambios Main

--- src/main/java/com/practica/cajanegra/Main.java
+++ src/main/java/com/practica/cajanegra/Main.java
@@ -8,16 +8,19 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		BinaryTree<String> arbol1 = new BinaryTree<String>("1");
+		BinaryTree<String> arbol2 = new BinaryTree<String>("2");
 		Node<String> nodo = arbol1.insert("2", arbol1.getRoot(),true);
 		arbol1.insert("5", arbol1.getRoot(), false);
 		arbol1.insert("3", nodo, true);
 		arbol1.insert("4", nodo, false);
 		ArrayList<String> lista = new ArrayList<>();
 		Iterator<String> itr = arbol1.iterator();
-		while(itr.hasNext()) {
-			lista.add(itr.next());
+		arbol2.remove(arbol2.getRoot());
+		Iterator<String> itr1 = arbol2.iterator();
+		while(itr1.hasNext()) {
+			lista.add(itr1.next());
 		}
-		System.out.println(lista.toString());
+		System.out.println(lista.toString()+" "+arbol2.getRoot());
 	}
 
 }
